a552e747e8bbf8615c0fd040f8166105858ecf7c
francitoshi <francitoshi@gmail.com>
2024-09-04 14:47:19 +0200

    update to gradle 8.2, fix deprecated stuff in gradle

--- src/test/java/io/nut/base/crypto/AtbashTest.java
+++ src/test/java/io/nut/base/crypto/AtbashTest.java
@@ -4,21 +4,21 @@
  */
 package io.nut.base.crypto;
 
+import static io.nut.base.util.Assert.assertEquals;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
 /**
  *
  * @author franci
  */
-public class BlaBlaBlaTest
+public class AtbashTest
 {
     
-    public BlaBlaBlaTest()
+    public AtbashTest()
     {
     }
     
