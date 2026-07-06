893e8d54c904a0831928b1cdce5d5da3c3a7eb1a
francitoshi <francitoshi@gmail.com>
2026-07-06 18:48:14 +0200

    overload Profiler.Task.count() with count(int n) to count n with just 1 call

--- src/main/java/io/nut/base/profile/Profiler.java
+++ src/main/java/io/nut/base/profile/Profiler.java
@@ -1,22 +1,7 @@
 /*
- *  Profiler.java
- *
- *  Copyright (C) 2023-2025 francitoshi@gmail.com
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- *  Report bugs or new features to: francitoshi@gmail.com
+ * Copyright (C) 2023-2026 francitoshi@gmail.com
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ * See LICENSE file in the project root for full license text.
  */
 package io.nut.base.profile;
 
@@ -96,6 +81,32 @@ public class Profiler
                 this.accumNanos += this.stepNanos;
                 this.stepNanos = 0L;
                 this.counter++;
+            }
+        }
+        
+        public void count(int steps)
+        {
+            if(steps<=0)
+            {
+                throw new IllegalArgumentException("steps must be >0 but is "+steps);
+            }
+            synchronized (lock)
+            {
+                if (this.active)
+                {
+                    this.stopNanos = System.nanoTime();
+                    this.stepNanos += this.stopNanos - this.startNanos;
+                    this.startNanos = this.stopNanos;
+                }
+                else
+                {
+                    this.startNanos = this.stopNanos = System.nanoTime();
+                }
+                this.minNanos = Math.min(this.stepNanos/steps, this.minNanos);
+                this.maxNanos = Math.max(this.stepNanos/steps, this.maxNanos);
+                this.accumNanos += this.stepNanos;
+                this.stepNanos = 0L;
+                this.counter += steps;
             }
         }
         
