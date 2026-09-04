96cc487bdfc15d711bea9c4d76561a15a9a6efda
francitoshi <francitoshi@gmail.com>
2026-09-04 15:06:10 +0200

    fix Table

--- src/main/java/io/nut/base/text/Table.java
+++ src/main/java/io/nut/base/text/Table.java
@@ -1,22 +1,7 @@
 /*
- * Table.java
- *
- * Copyright (c) 2024-2025 francitoshi@gmail.com
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- *  Report bugs or new features to: francitoshi@gmail.com
+ * Copyright (C) 2024-2026 francitoshi@gmail.com
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ * See LICENSE file in the project root for full license text.
  */
 package io.nut.base.text;
 
@@ -64,7 +49,7 @@ public final class Table
     private final String[] colNames;
     private final String[] rowNames;
     
-    private volatile Paint paint = Paint.Space;
+    private volatile Paint paint = Paint.Math;
 
     public Table(int rows, int cols, boolean alignRight)
     {
