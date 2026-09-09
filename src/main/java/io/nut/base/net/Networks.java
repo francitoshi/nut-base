341abb4602e72511b5363041407616b835e1ed28
francitoshi <francitoshi@gmail.com>
2026-09-09 17:03:32 +0200

    fix misprint in Networks.LANS

--- src/main/java/io/nut/base/net/Networks.java
+++ src/main/java/io/nut/base/net/Networks.java
@@ -1,22 +1,7 @@
 /*
- * Networks.java
- *
- * Copyright (c) 2011-2026 francitoshi@gmail.com
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- *  Report bugs or new features to: francitoshi@gmail.com
+ * Copyright (C) 2011-2026 francitoshi@gmail.com
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ * See LICENSE file in the project root for full license text.
  */
 package io.nut.base.net;
 
@@ -46,13 +31,13 @@ public class Networks
         { "10.",     "10.0.0.0",   "10.255.255.255"},
         { "172.16.", "172.16.0.0", "172.16.255.255"},
         { "172.17.", "172.17.0.0", "172.17.255.255"},
-        { "172.18.", "172.18.0.0", "172.186.255.255"},
+        { "172.18.", "172.18.0.0", "172.18.255.255"},
         { "172.19.", "172.19.0.0", "172.19.255.255"},
         { "172.20.", "172.20.0.0", "172.20.255.255"},
         { "172.21.", "172.21.0.0", "172.21.255.255"},
         { "172.22.", "172.22.0.0", "172.22.255.255"},
         { "172.23.", "172.23.0.0", "172.23.255.255"},
-        { "172.24.", "172.23.0.0", "172.24.255.255"},
+        { "172.24.", "172.24.0.0", "172.24.255.255"},
         { "172.25.", "172.25.0.0", "172.25.255.255"},
         { "172.26.", "172.26.0.0", "172.26.255.255"},
         { "172.27.", "172.27.0.0", "172.27.255.255"},
