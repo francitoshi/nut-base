802e68134639b700986da1923dea96b4174b8eee
francitoshi <francitoshi@gmail.com>
2026-09-09 16:54:53 +0200

    fix Bug counting EOF in MeteredInputStream

--- src/main/java/io/nut/base/io/MeteredInputStream.java
+++ src/main/java/io/nut/base/io/MeteredInputStream.java
@@ -1,22 +1,7 @@
 /*
- *  MeteredInputStream.java
- *
- *  Copyright (c) 2017-2024 francitoshi@gmail.com
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- *  Report bugs or new features to: francitoshi@gmail.com
+ * Copyright (C) 2017-2026 francitoshi@gmail.com
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ * See LICENSE file in the project root for full license text.
  */
 package io.nut.base.io;
 
@@ -58,7 +43,10 @@ public class MeteredInputStream extends InputStream
     public int read() throws IOException
     {
         int b = this.in.read();
-        this.readCount.incrementAndGet();
+        if(b>=0)
+        {
+            this.readCount.incrementAndGet();
+        }
         return b;
     }
 
@@ -66,7 +54,10 @@ public class MeteredInputStream extends InputStream
     public int read(byte[] bytes) throws IOException
     {
         int count = this.in.read(bytes);
-        this.readCount.addAndGet(count);
+        if(count>0)
+        {
+            this.readCount.addAndGet(count);
+        }
         return count;
     }
 
@@ -74,7 +65,10 @@ public class MeteredInputStream extends InputStream
     public int read(byte[] bytes, int off, int len) throws IOException
     {
         int count = this.in.read(bytes, off, len);
-        this.readCount.addAndGet(count);
+        if(count>0)
+        {
+            this.readCount.addAndGet(count);
+        }
         return count;
     }
 
